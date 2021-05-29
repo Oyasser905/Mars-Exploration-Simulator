058a3e76a93eaa59d3f0b51a9c2f53db48e3f3a1
Oyasser905 <76390560+Oyasser905@users.noreply.github.com>
2021-05-29 15:07:25 +0200

    Output functions added to UI.cpp

--- Mars_Exp_DS/UI.cpp
+++ Mars_Exp_DS/UI.cpp
@@ -8,6 +8,7 @@
 #include"Rover.h"
 #include"FormulationEvent.h"
 #include"priorityQueue.h"
+#include"windows.h"
 using namespace std;
 
 int num_pr; //No. of polar rovers
@@ -69,7 +70,7 @@ void r_input(string file)
     if (fptr.is_open())
     {
         cout << "\nFile opened successfully...\nFile Contents...\n\n";
-        while (cin.get())
+        while (cin.get() && !fptr.eof())
         {
             char event_type; //Event type (formulation, cancellation, promotion)
             char rover_type; //Type of rover (mountainous, polar, emergency)
@@ -90,11 +91,11 @@ void r_input(string file)
             {
                 fptr >> event_type >> rover_type >> event_day >> ID >> tloc >> mdur >> sig;
 
-                if (event_type == 'F')
+                /*if (event_type == 'F')
                 {
-                    /*Event* e = new FormulationEvent();
-                    e->Execute(rover_type, event_day, ID, tloc, mdur, sig, EM, PM);*/
-                }
+                    Event* e = new FormulationEvent();
+                    e->Execute(rover_type, event_day, ID, tloc, mdur, sig, EM, PM);
+                }*/
             }
             /*for (int i = 0; i < num_er; i++)
             {
@@ -105,9 +106,9 @@ void r_input(string file)
             {
                 Rover* pr = new Rover(rover_type, er_sp, er_ch, num_missions);
                 PR->enqueue(pr, pr_sp);
-            }*/
+            }
+            Assign_M_to_R(EM, PM, ER, PR, WL);*/
 
-            //Assign_M_to_R(EM, PM, ER, PR, WL);
             cout <<
             num_pr << " " << num_er << "\n" <<
             pr_sp << " " << er_sp << "\n" <<
@@ -120,6 +121,7 @@ void r_input(string file)
             }
         }
         fptr.close();
+        cout << "File successfully closed...\n";
     }
     else
     {
@@ -127,10 +129,106 @@ void r_input(string file)
     }
 }
 
+void Interactive_mode()
+{
+    char x;
+    for (int d = 0; d < 2; d++)
+    {
+        int w_m = 7, i_e = 4, a_r = 4, i_c = 2, c_m = 3;
+        cout << "Current Day: " << d << "\n";
+        cout << w_m << " Waiting Missions: " << "[11, 13] (6, 8)\n";
+        cout << "------------------------------------------\n";
+        cout << a_r << " In-Execution Missions/Rovers: " << "[2/1, 10/7] (3/5)\n";
+        cout << "------------------------------------------\n";
+        cout << w_m << " Available Rovers: " << "[4, 6] (10)\n";
+        cout << "------------------------------------------\n";
+        cout << i_c << " In-Checkup Rovers: " << "[2] (3)\n";
+        cout << "------------------------------------------\n";
+        cout << c_m << " Completed Missions: " << "(4) [1]\n\n\n";
+        //cin.get(x);
+        cin >> x;
+    }
+}
+
+void SbS_mode()
+{
+    for (int d = 0; d < 2; d++)
+    {
+        int w_m = 7, i_e = 4, a_r = 4, i_c = 2, c_m = 3;
+        cout << "Current Day: " << d << "\n";
+        cout << w_m << " Waiting Missions: " << "[11, 13] (6, 8)\n";
+        cout << "------------------------------------------\n";
+        cout << a_r << " In-Execution Missions/Rovers: " << "[2/1, 10/7] (3/5)\n";
+        cout << "------------------------------------------\n";
+        cout << w_m << " Available Rovers: " << "[4, 6] (10)\n";
+        cout << "------------------------------------------\n";
+        cout << i_c << " In-Checkup Rovers: " << "[2] (3)\n";
+        cout << "------------------------------------------\n";
+        cout << c_m << " Completed Missions: " << "(4) [1]\n\n\n";
+        Sleep(3000);
+    }
+}
+
+void Silent_mode()
+{
+    cout << "Silent Mode\nSimulation Starts...\nSimulation Ends, Output file created\n";
+}
+
+void w_file(string fname)
+{
+    ofstream myfile(fname);
+    if (myfile.is_open())
+    {
+        myfile << "This is a line.\n";
+        myfile << "This is another line.\n";
+        myfile.close();
+    }
+    else cout << "Unable to write to file...";
+}
+
+
+void p_output(string fname)
+{
+    int choice;
+    while (true)
+    {
+        system("CLS");
+        cout << "\n1.Interactive mode\n2.Step-by-Step mode\n3.Silent mode\n4.Exit Program\n\n";
+        cin >> choice;
+        if (choice == 1)
+        {
+            w_file(fname);
+            Interactive_mode();
+        }
+        else if (choice == 2)
+        {
+            w_file(fname);
+            SbS_mode();
+        }
+        else if (choice == 3)
+        {
+            w_file(fname);
+            Silent_mode();
+        }
+        else if (choice == 4)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Error! Incorrect input re-enter your choice...\n\n";
+        }
+    }
+}
+
 int main()
 {
-    string file;
+    string i_file, o_file;
     cout << "(Please include file extension)\nEnter file name: ";
-    cin >> file;
-    r_input(file);
+    cin >> i_file;
+    r_input(i_file);
+
+    cout << "(Please include file extension)\nEnter file name: ";
+    cin >> o_file;
+    p_output(o_file);
 }
