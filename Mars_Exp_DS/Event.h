fee8712840c80249737e19be2f60dfa9c5f20cdd
Oyasser905 <76390560+Oyasser905@users.noreply.github.com>
2021-06-02 22:29:19 +0200

    fixed bug

--- Mars_Exp_DS/Event.h
+++ Mars_Exp_DS/Event.h
@@ -1,14 +1,14 @@
 #pragma once
 #include"Mission.h"
 #include"priorityQueue.h"
-
+#include"Queue.h"
 class Event
 {
 private:
 	int missionID;
 	int eventDay;
 public:
-	virtual void Execute(char rover_type, int event_day, int ID, int tloc, int mdur, int sig, PriorityQueue<Mission*>*& EM, PriorityQueue<Mission*>*& PM) = 0;
+	virtual void Execute(char rover_type, int event_day, int ID, int tloc, int mdur, int sig, PriorityQueue<Mission*>*& EM, LinkedQueue<Mission*>*& PM) = 0;
 	int getDay() const;
 	int getID() const;
 };
